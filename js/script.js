afb2a98e1bf8629635b87a60fe6c7f9cccd1dbe3
manuelatrotta <manuelat88@hotmail.it>
2020-01-27 11:54:14 +0100

    funzione che ricerca testo mediante keyup

--- js/script.js
+++ js/script.js
@@ -3,13 +3,28 @@ $(document).ready(function () {
   $('.icon-send').click(function () {
     sendMessage();
   });
-});
-//invio messaggio anche da tastiera enter= 13
-$(document).keydown(function () {
-  console.log(event.which);
-  if(event.keyCode == 13) {
-   sendMessage();
-  }
+
+  $('.send-message').keypress(function(event) {
+    if(event.which == 13) {
+      sendMessage();
+    }
+  });
+//funzione che cerca i nomi nella chatlist mediante pressione dei pulsanti(keyup)
+  $('.contact-search input').keyup(function () {
+
+    var text = $('.contact-search input').val().toLowerCase();
+//variabile in cui salvo il testo immesso e con toLowerCase trasformo tutti in minuscolo
+    $('.contact-element').each(function () {
+      var contactName = $(this).find('.contact-name').text().toLowerCase();
+  //condizione in cui verifichiamo se il testo inserito è presente nei nomi della chat
+      if(contactName.includes(text) == true) {
+        // console.log('incluso');
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  });
 });
 //funzione in cui se il messaggio ha una lunghezza !=0 si clona
 function sendMessage() {
@@ -33,6 +48,7 @@ function sendMessage() {
     //con setTimeout richiamo la funzione e dopo 2 s parte messaggio automatico
     setTimeout(sendResponse, 2000);
     $('input.send-message').val('');
+
   }
 }
 //funzione che invia un messaggio automatico come risposta
@@ -47,6 +63,23 @@ function sendResponse() {
   messageResponse.addClass('received');
   $('.col-right-messages.active').append(messageResponse);
 }
+
+//funzione che cerca i nomi nella chatlist mediante pressione dei pulsanti(keyup)
+$('.contact-search input').keyup(function () {
+//variabile in cui salvo il testo immesso e con toLowerCase trasformo tutti in minuscolo
+  var text = $('.contact-search input').val().toLowerCase();
+//.each funziona come un ciclo for
+  $('.contact-element').each(function () {
+    var contactName = $(this).find('.contact-name').text().toLowerCase();
+    //condizione in cui verifichiamo se il testo inserito è presente nei nomi della chat
+    if(contactName.includes(text) == true) {
+      // console.log('incluso');
+      $(this).show();
+    } else {
+      $(this).hide();
+    }
+  });
+});
 //funzione che aggiunge 0
 function addZero(number) {
   if(number < 10) {
